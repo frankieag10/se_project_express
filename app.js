9ed4c10672240319b525632597898eb247f4fdfc
frankieag10 <frankieag10@gmail.com>
2023-07-18 09:51:13 -0700

    sprint 12 updates

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
