c1b9628d5d34dac693d90931cad12bcfef9110c2
frankieag10 <frankieag10@gmail.com>
2023-07-18 11:22:27 -0700

    sprint 12 updates

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const mongoose = require("mongoose");
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/wtwr_db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.error("Failed to connect to MongoDB:", err);
+    } else {
+      console.log("Successfully connected to MongoDB");
+    }
+  },
+);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
---
+++ models/clothingItems.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => validator.isURL(value),
+      message: "You must enter a valid URL for the avatar.",
+    },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => validator.isURL(value),
+      message: "You must enter a valid URL for the image URL.",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "User",
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ClothingItem = mongoose.model("ClothingItem", clothingItemSchema);
+
+module.exports = { User, ClothingItem };
