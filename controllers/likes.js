db6c05f024f91c4e6cebb86171ea73e967a474ee
frankieag10 <frankieag10@gmail.com>
2023-07-24 13:04:21 -0700

    sprint 12 updates

--- controllers/likes.js
+++ controllers/likes.js
@@ -1,29 +1,32 @@
 const clothingItem = require("../models/clothingItems");
 const { handleError } = require("../utils/config");
 
-module.exports.likeItem = (req, res) => {
-  clothingItem
-    .findByIdAndUpdate(
-      req.params.itemId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true }
-    )
-    .orFail()
-    .then((data) => res.status(200).send(data))
-    .catch((err) => {
-      handleError(req, res, err);
-    });
+module.exports.likeItem = async (req, res) => {
+  try {
+    const data = await clothingItem
+      .findByIdAndUpdate(
+        req.params.itemId,
+        { $addToSet: { likes: req.user._id } },
+        { new: true }
+      )
+      .orFail();
+    res.status(200).send(data);
+  } catch (err) {
+    handleError(req, res, err);
+  }
 };
 
-module.exports.dislikeItem = (req, res) =>
-  clothingItem
-    .findByIdAndUpdate(
-      req.params.itemId,
-      { $pull: { likes: req.user._id } },
-      { new: true }
-    )
-    .orFail()
-    .then((data) => res.status(200).send(data))
-    .catch((err) => {
-      handleError(req, res, err);
-    });
+module.exports.dislikeItem = async (req, res) => {
+  try {
+    const data = await clothingItem
+      .findByIdAndUpdate(
+        req.params.itemId,
+        { $pull: { likes: req.user._id } },
+        { new: true }
+      )
+      .orFail();
+    res.status(200).send(data);
+  } catch (err) {
+    handleError(req, res, err);
+  }
+};
