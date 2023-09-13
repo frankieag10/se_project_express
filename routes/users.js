7114e4d481f043885d290c4092a6b88c0657d3d5
frankieag10 <frankieag10@gmail.com>
2023-09-13 13:19:26 -0700

    sprint 15 updates

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-const router = require("express").Router();
+/*const router = require("express").Router();
 const auth = require("../middlewares/auth");
 
 const { getCurrentUser, updateUser } = require("../controllers/users");
 
 router.get("/users/me", auth, getCurrentUser);
+router.patch("/users/me", auth, updateUser);
+
+module.exports = router;
+*/
+
+const router = require("express").Router();
+const auth = require("../middlewares/auth");
+const { updateUser } = require("../controllers/users");
+
+// Define the update user route with the appropriate HTTP method and callback function.
 router.patch("/users/me", auth, updateUser);
 
 module.exports = router;
