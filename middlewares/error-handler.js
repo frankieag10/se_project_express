3da034a2687345e15cd6303b36202e83753bec1a
frankieag10 <frankieag10@gmail.com>
2023-09-13 15:05:08 -0700

    sprint 15 updates

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,4 +1,4 @@
-const errorHandler = (err, req, res, next) => {
+/*const errorHandler = (err, req, res, next) => {
   if (err.code === 11000) {
     return res.status(409).send({
       message: "Email already exists. Please choose a different one.",
@@ -11,6 +11,28 @@ const errorHandler = (err, req, res, next) => {
     return res.status(400).send({ errors });
   } else {
     console.error(err);
+    return res.status(500).send({ message: "Internal Server Error" });
+  }
+};
+*/
+
+const errorHandler = (err, req, res, next) => {
+  console.log("Error Handler:", err);
+
+  if (err.code === 11000) {
+    console.log("Duplicate Key Error");
+    return res.status(409).send({
+      message: "Email already exists. Please choose a different one.",
+    });
+  } else if (err.name === "ValidationError") {
+    console.log("Validation Error");
+    const errors = Object.keys(err.errors).map((key) => ({
+      field: key,
+      message: err.errors[key].message,
+    }));
+    return res.status(400).send({ errors });
+  } else {
+    console.error("Internal Server Error:", err);
     return res.status(500).send({ message: "Internal Server Error" });
   }
 };
