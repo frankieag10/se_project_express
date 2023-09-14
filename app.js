be0854b670215e467ec9f0b18afbba2565b3ae6e
frankieag10 <frankieag10@gmail.com>
2023-09-13 17:49:50 -0700

    sprint 15 updates

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.listen(PORT, () => {
 });
 */
 
-const express = require("express");
+/*const express = require("express");
 const mongoose = require("mongoose");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
@@ -75,6 +75,48 @@ app.use(limiter);
 app.use(helmet());
 app.use(errorHandler);
 app.use(errors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App started on port: ${PORT}`);
+});
+*/
+
+const express = require("express");
+const mongoose = require("mongoose");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const cors = require("cors");
+const routes = require("./routes");
+const { loginUser, createUser } = require("./controllers");
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const { PORT = 3001 } = process.env;
+const app = express();
+app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
+app.use(cors());
+app.use(helmet());
+app.post("/signin", loginUser);
+app.post("/signup", createUser);
 
 app.use(
   cors({
