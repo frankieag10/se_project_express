b7f31a88b5a1c1f117cf6a2fb8f23532ce4143c4
frankieag10 <frankieag10@gmail.com>
2023-09-23 17:10:37 -0700

    sprint 15 updates

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+/*const router = require("express").Router();
 //update
 const clothingItem = require("./clothingItems");
 const User = require("./users");
@@ -12,6 +12,41 @@ router.use("/", clothingItem);
 router.use("/", User);
 router.use("/", itemRouter);
 router.use("/", likesRouter);
+
+router.post("/signup", createUser);
+router.post("/signin", loginUser);
+
+router.get("/users", (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith("Bearer")) {
+    res.status(400).send("Authorization is still required\n");
+  } else {
+    res.send(User);
+  }
+});
+
+router.use((req, res) => {
+  res
+    .status(ERROR_404)
+    .send({ message: "The requested resource was not found" });
+});
+
+module.exports = router;
+*/
+
+const router = require("express").Router();
+const { ERROR_404 } = require("../utils/errors");
+const { createUser, loginUser } = require("../controllers/users");
+
+// Import route modules
+const clothingItemRouter = require("./clothingItems");
+const userRouter = require("./users");
+const likeRouter = require("./likes");
+
+// Define routes
+router.use("/clothingItems", clothingItemRouter);
+router.use("/users", userRouter);
+router.use("/likes", likeRouter);
 
 router.post("/signup", createUser);
 router.post("/signin", loginUser);
