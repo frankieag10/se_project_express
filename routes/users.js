00562586b827343c94e8bca46d01fe4cab132ef6
frankieag10 <frankieag10@gmail.com>
2023-09-26 19:56:22 -0700

    sprint 15 updates

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ const { getCurrentUser, updateUser } = require("../controllers/users");
 const { clothingItemValidation } = require("../middlewares/validation");
 
 //user route with the appropriate HTTP method and callback function.
-router.get("users/me", auth, getCurrentUser);
+router.get("/users/me", auth, getCurrentUser);
 console.log({ auth, clothingItemValidation, updateUser });
-router.patch("users/me", auth, clothingItemValidation, updateUser);
+router.patch("/users/me", auth, clothingItemValidation, updateUser);
 
 module.exports = router;
