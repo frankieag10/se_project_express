32f7fffe2e0e66b097abae5edcd6b3591dff62a6
frankieag10 <frankieag10@gmail.com>
2023-09-27 10:51:11 -0700

    env update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ const NotFoundError = require("../errors/not-found-error");
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 const likesRouter = require("./clothingItems");
+const { loginUser, createUser } = require("../controllers/users");
+const {
+  logInValidation,
+  userInfoValidation,
+} = require("../middlewares/validation");
 
+router.post("/signin", logInValidation, loginUser);
+router.post("/signup", userInfoValidation, createUser);
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
 router.use("/likes", likesRouter);
