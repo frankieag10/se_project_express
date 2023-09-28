9e360d09d2d42f55f757bbb1f367edd259d3abfa
frankieag10 <frankieag10@gmail.com>
2023-09-27 21:27:37 -0700

    index.js console.logs

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,36 @@ const {
   userInfoValidation,
 } = require("../middlewares/validation");
 
+// Add a console log for route entry
+router.use((req, res, next) => {
+  console.log(`Entering route: ${req.url}`);
+  next();
+});
+
 router.post("/signin", logInValidation, loginUser);
 router.post("/signup", userInfoValidation, createUser);
 router.use("/items", itemRouter);
 router.use("/users", userRouter);
+
+// Add a console log for route entry
+router.use(
+  "/items",
+  (req, res, next) => {
+    console.log("Entering /items route");
+    next();
+  },
+  itemRouter
+);
+
+// Add a console log for route entry
+router.use(
+  "/users",
+  (req, res, next) => {
+    console.log("Entering /users route");
+    next();
+  },
+  userRouter
+);
 
 router.use(() => {
   throw new NotFoundError(`The page you're looking for not found`);
