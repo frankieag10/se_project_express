43c6aa841a39ec59723b6163a8c66201d16c4c05
frankieag10 <frankieag10@gmail.com>
2023-09-28 15:10:43 -0700

    clothingItem.js  updates

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -14,31 +14,31 @@ const {
 } = require("../middlewares/validation");
 
 // GET /items — returns all clothing items
-router.get("/items", (req, res) => {
+router.get("/", (req, res) => {
   console.log("GET /items"); // Added console.log
   getClothingItem(req, res);
 });
 
 // POST /items — creates a new item
-router.post("/items", auth, (req, res) => {
+router.post("/", auth, (req, res) => {
   console.log("POST /items"); // Added console.log
   clothingItemValidation(req, res, () => createClothingItem(req, res));
 });
 
 // DELETE /items/:itemId — deletes an item by _id
-router.delete("/items/:itemId", auth, (req, res) => {
+router.delete("/:itemId", auth, (req, res) => {
   console.log("DELETE /items/:itemId"); // Added console.log
   idValidation(req, res, () => deleteClothingItem(req, res));
 });
 
 // PUT /items/:itemId/likes — like an item
-router.put("/items/:itemId/likes", auth, (req, res) => {
+router.put("/:itemId/likes", auth, (req, res) => {
   console.log("PUT /items/:itemId/likes"); // Added console.log
   idValidation(req, res, () => likeItem(req, res));
 });
 
 // DELETE /items/:itemId/likes — unlike an item
-router.delete("/items/:itemId/likes", auth, (req, res) => {
+router.delete("/:itemId/likes", auth, (req, res) => {
   console.log("DELETE /items/:itemId/likes"); // Added console.log
   idValidation(req, res, () => dislikeItem(req, res));
 });
