eed6b3925aaa241760818b7cdf5836863c526020
frankieag10 <frankieag10@gmail.com>
2023-09-28 14:46:24 -0700

    users.js uupdates

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,10 @@ const auth = require("../middlewares/auth");
 const { getCurrentUser, updateUser } = require("../controllers/users");
 const { clothingItemValidation } = require("../middlewares/validation");
 
-console.log("GET /users/me", { auth, getCurrentUser });
-router.get("/users/me", auth, getCurrentUser);
+console.log("GET /me", { auth, getCurrentUser });
+router.get("/me", auth, getCurrentUser);
 
-console.log("PATCH /users/me", { auth, clothingItemValidation, updateUser });
-router.patch("/users/me", auth, clothingItemValidation, updateUser);
+console.log("PATCH /me", { auth, clothingItemValidation, updateUser });
+router.patch("/me", auth, clothingItemValidation, updateUser);
 
 module.exports = router;
