1472f45baf3cca57c0f2cc2357b84424d8935990
frankieag10 <frankieag10@gmail.com>
2023-09-27 21:47:44 -0700

    clothing itmes/users.js  console.logs

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,10 @@ const auth = require("../middlewares/auth");
 const { getCurrentUser, updateUser } = require("../controllers/users");
 const { clothingItemValidation } = require("../middlewares/validation");
 
-//user route with the appropriate HTTP method and callback function.
+console.log("GET /users/me", { auth, getCurrentUser });
 router.get("/users/me", auth, getCurrentUser);
-console.log({ auth, clothingItemValidation, updateUser });
+
+console.log("PATCH /users/me", { auth, clothingItemValidation, updateUser });
 router.patch("/users/me", auth, clothingItemValidation, updateUser);
 
 module.exports = router;
